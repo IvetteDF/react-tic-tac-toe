d23dbf675c5763b39c5d167f22a9b89aed267902
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2021-12-22 12:25:11 -0500

    updated winner message

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ const App = () => {
     <div className='App'>
       <header className='App-header'>
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is {winnerState} </h2>
+        <h2>Winner is {winnerState}</h2>
         <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
